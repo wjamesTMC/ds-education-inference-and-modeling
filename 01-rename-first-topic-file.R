6cf7d562d89533c6ae45d5e228bef05cc2603416
wjamesTMC <jamesw@csps.com>
2018-09-25 07:37:59 -0400

    Starting infeence and modeling

---
+++ 01-rename-first-topic-file.R
@@ -0,0 +1,17 @@
+# --------------------------------------------------------------------------------
+#
+# <title>
+#
+# --------------------------------------------------------------------------------
+
+# Setup
+library(tidyverse)
+library(dslabs)
+library(dplyr)
+library(ggplot2)
+library(ggthemes)
+library(ggrepel)
+library(gridExtra)
+library(RColorBrewer)
+library(Lahman)
+library(HistData)
